e05198f01f0d5611b23b7516fcb1ae1196138d24
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2024-05-11 00:16:40 -0500

    week 3 started

---
+++ week3/array1.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class array1 {
+    public static void main(String[] args) {
+        System.out.println("the program has started");
+        String fruits[]= {"apple", "oranges","bananas"};
+        String vegetables[]= {"cauliflower","stringBeans","Mushrooms"};
+        System.out.println(fruits[0]);
+        for(int i=0; i<= vegetables.length-1;i++){
+            System.out.println(vegetables[i]);
+        }
+    }
+}
